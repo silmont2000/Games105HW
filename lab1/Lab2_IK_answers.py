d681ad777a02a1c7a560975ffe36844657aa6af3
xieboyang.xsilmont <xieboyang.xsilmont@bytedance.com>
2023-06-01 19:59:38 +0800

    有点意思了

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -1,6 +1,23 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+
+def rotation_matrix(a, b):
+    # 计算旋转轴
+    n = np.cross(a, b)
+    # 计算夹角
+    cos_theta = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    sin_theta = np.linalg.norm(n) / (np.linalg.norm(a) * np.linalg.norm(b))
+    theta = np.arctan2(sin_theta, cos_theta)
+    # 构造旋转矩阵
+    c = np.cos(theta)
+    s = np.sin(theta)
+    v = 1 - c
+    rotation_matrix = np.array([[n[0]*n[0]*v+c, n[0]*n[1]*v-n[2]*s, n[0]*n[2]*v+n[1]*s],
+                                 [n[0]*n[1]*v+n[2]*s, n[1]*n[1]*v+c, n[1]*n[2]*v-n[0]*s],
+                                 [n[0]*n[2]*v-n[1]*s, n[1]*n[2]*v+n[0]*s, n[2]*n[2]*v+c]])
+    return rotation_matrix
+
 def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, target_pose):
     """
     完成函数，计算逆运动学
@@ -14,8 +31,30 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
         joint_positions: 计算得到的关节位置，是一个numpy数组，shape为(M, 3)，M为关节数
         joint_orientations: 计算得到的关节朝向，是一个numpy数组，shape为(M, 4)，M为关节数
     """
-    
-    
+    path,path_name,path1,path2=meta_data.get_path_from_root_to_end()
+    path_end=path1[0]+1 ## lWrist_end
+    for idx in range(0,len(path1)):
+        path_joint_id=path1[idx]
+        next_joint_id=0
+        if idx==0:
+            next_joint_id=path_end
+        else:
+            next_joint_id=path1[idx-1]
+
+        vec_to_end=joint_positions[path_joint_id]-joint_positions[path_end]
+        vec_to_target=joint_positions[path_joint_id]-target_pose
+        rot_matrix=rotation_matrix(vec_to_target,vec_to_end)
+
+        initial_orientation=R.from_quat(joint_orientations[path_joint_id]).as_matrix()
+        rot_matrix_R=R.from_matrix(rotation_matrix(vec_to_end,vec_to_target)).as_matrix()
+        calculated_orientation=rot_matrix_R.dot(initial_orientation)
+        joint_orientations[path_joint_id]=R.from_matrix(calculated_orientation).as_quat()
+
+        if idx==1:
+            vec_to_next=joint_positions[next_joint_id]-joint_positions[path_joint_id]
+            calculated_vec_to_next=rot_matrix.dot(vec_to_next)
+            joint_positions[next_joint_id]=calculated_vec_to_next+joint_positions[path_joint_id]
+
     return joint_positions, joint_orientations
 
 def part2_inverse_kinematics(meta_data, joint_positions, joint_orientations, relative_x, relative_z, target_height):
